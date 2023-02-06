e2af5fb3e359b9a0dd262d749db1c21e127c6f0e
comar <saywooong@gmail.com>
2023-02-06 10:36:25 +0900

    feat: DenoiseDataset

--- edit_dataset.py
+++ edit_dataset.py
@@ -9,9 +9,11 @@ from os.path import join
 import numpy as np
 import torch
 import torchvision
+from torchvision.transforms import ToPILImage, ToTensor
 from einops import rearrange
 from PIL import Image
 from torch.utils.data import Dataset
+import random
 
 
 class EditDataset(Dataset):
@@ -189,3 +191,83 @@ class LowlightDataset(Dataset):
         return dict(edited=image_1,
                     edit=dict(c_concat=image_0, c_crossattn=prompt))
 
+
+class DenoiseDataset(Dataset):
+
+    def __init__(
+        self,
+        path: str,
+        split: str = "train",
+        splits: tuple[float, float, float] = (0.9, 0.05, 0.05),
+        min_resize_res: int = 256,
+        max_resize_res: int = 256,
+        crop_res: int = 256,
+        flip_prob: float = 0.0,
+    ):
+        assert split in ("train", "val", "test")
+        assert sum(splits) == 1
+        self.path = path
+        self.min_resize_res = min_resize_res
+        self.max_resize_res = max_resize_res
+        self.crop_res = crop_res
+        self.flip_prob = flip_prob
+
+        with open(Path(self.path, "seeds.json")) as f:
+            self.seeds = json.load(f)
+
+        split_0, split_1 = {
+            "train": (0.0, splits[0]),
+            "val": (splits[0], splits[0] + splits[1]),
+            "test": (splits[0] + splits[1], 1.0),
+        }[split]
+
+        idx_0 = math.floor(split_0 * len(self.seeds))
+        idx_1 = math.floor(split_1 * len(self.seeds))
+        self.seeds = self.seeds[idx_0:idx_1]
+
+    def __len__(self) -> int:
+        return len(self.seeds)
+
+    def add_noise(self, im: Image, sigma: float):
+        x = ToTensor()(im)
+        n = torch.randn_like(x)
+        n.normal_(mean=0, std=sigma)
+        x = x + n
+        x = x.clamp(0, 1)
+        x = ToPILImage()(x)
+        return x
+
+    def __getitem__(self, i: int) -> dict[str, Any]:
+        path = self.seeds[i]
+        path = join(self.path, path)
+        prompt = "denoise"
+
+        image_0 = Image.open(path)
+        image_1 = Image.open(path)
+
+        # Add noise
+        sigma = random.random() / 2
+        image_0 = self.add_noise(image_0, sigma)
+
+        reize_res = torch.randint(self.min_resize_res, self.max_resize_res + 1,
+                                  ()).item()
+        image_0 = image_0.resize((reize_res, reize_res),
+                                 Image.Resampling.LANCZOS)
+        image_1 = image_1.resize((reize_res, reize_res),
+                                 Image.Resampling.LANCZOS)
+
+        image_0 = rearrange(
+            2 * torch.tensor(np.array(image_0)).float() / 255 - 1,
+            "h w c -> c h w")
+        image_1 = rearrange(
+            2 * torch.tensor(np.array(image_1)).float() / 255 - 1,
+            "h w c -> c h w")
+
+        crop = torchvision.transforms.RandomCrop(self.crop_res)
+        flip = torchvision.transforms.RandomHorizontalFlip(
+            float(self.flip_prob))
+        exit()
+        image_0, image_1 = flip(crop(torch.cat((image_0, image_1)))).chunk(2)
+
+        return dict(edited=image_1,
+                    edit=dict(c_concat=image_0, c_crossattn=prompt))
