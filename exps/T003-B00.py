b7a548b112704aac77a05853a5a73fe955ad34da
comar <saywooong@gmail.com>
2023-08-28 17:20:33 +0900

    T003-B00

--- exps/T003-B00.py
+++ exps/T003-B00.py
@@ -1,6 +1,4 @@
 import argparse, os, sys, datetime, glob
-from torchvision.transforms import ToPILImage
-from torch.utils.data import DataLoader
 from PIL import Image
 import json
 from pytz import timezone
@@ -504,6 +502,10 @@ class UNetModel(nn.Module):
                 )
             ]
         )
+        self.middle_block = TimestepEmbedSequential(conv_nd(dims, 640, 1280, 3, padding=1))
+        self.middle_block = conv_nd(dims, 640, 1280, 3, padding=1)
+        # self.middle_block = nn.Conv2d(640, 1280, 3, 1, 1)
+
         self._feature_size = model_channels
         input_block_chans = [model_channels]
         ch = model_channels
@@ -578,33 +580,33 @@ class UNetModel(nn.Module):
         if legacy:
             #num_heads = 1
             dim_head = ch // num_heads if use_spatial_transformer else num_head_channels
-        self.middle_block = TimestepEmbedSequential(
-            ResBlock(
-                ch,
-                time_embed_dim,
-                dropout,
-                dims=dims,
-                use_checkpoint=use_checkpoint,
-                use_scale_shift_norm=use_scale_shift_norm,
-            ),
-            AttentionBlock(
-                ch,
-                use_checkpoint=use_checkpoint,
-                num_heads=num_heads,
-                num_head_channels=dim_head,
-                use_new_attention_order=use_new_attention_order,
-            ) if not use_spatial_transformer else SpatialTransformer(
-                            ch, num_heads, dim_head, depth=transformer_depth, context_dim=context_dim
-                        ),
-            ResBlock(
-                ch,
-                time_embed_dim,
-                dropout,
-                dims=dims,
-                use_checkpoint=use_checkpoint,
-                use_scale_shift_norm=use_scale_shift_norm,
-            ),
-        )
+        # self.middle_block = TimestepEmbedSequential(
+        #     ResBlock(
+        #         ch,
+        #         time_embed_dim,
+        #         dropout,
+        #         dims=dims,
+        #         use_checkpoint=use_checkpoint,
+        #         use_scale_shift_norm=use_scale_shift_norm,
+        #     ),
+        #     AttentionBlock(
+        #         ch,
+        #         use_checkpoint=use_checkpoint,
+        #         num_heads=num_heads,
+        #         num_head_channels=dim_head,
+        #         use_new_attention_order=use_new_attention_order,
+        #     ) if not use_spatial_transformer else SpatialTransformer(
+        #                     ch, num_heads, dim_head, depth=transformer_depth, context_dim=context_dim
+        #                 ),
+        #     ResBlock(
+        #         ch,
+        #         time_embed_dim,
+        #         dropout,
+        #         dims=dims,
+        #         use_checkpoint=use_checkpoint,
+        #         use_scale_shift_norm=use_scale_shift_norm,
+        #     ),
+        # )
         self._feature_size += ch
 
         self.output_blocks = nn.ModuleList([])
@@ -663,6 +665,9 @@ class UNetModel(nn.Module):
                 self.output_blocks.append(TimestepEmbedSequential(*layers))
                 self._feature_size += ch
 
+        # Remove
+        self._reform()
+
         self.out = nn.Sequential(
             normalization(ch),
             nn.SiLU(),
@@ -674,6 +679,7 @@ class UNetModel(nn.Module):
             conv_nd(dims, model_channels, n_embed, 1),
             #nn.LogSoftmax(dim=1)  # change to cross_entropy and produce non-normalized logits
         )
+
 
     def convert_to_fp16(self):
         """
@@ -711,12 +717,20 @@ class UNetModel(nn.Module):
             assert y.shape == (x.shape[0],)
             emb = emb + self.label_emb(y)
 
+        # del_encode_idxs = [6, 7, 8, 9, 10, 11]
+        # del_decode_idxs = [0, 1, 2, 3, 4, 5]
+
         h = x.type(self.dtype)
-        for module in self.input_blocks:
+        for i, module in enumerate(self.input_blocks):
+            # if i in del_encode_idxs:
+            #     continue
             h = module(h, emb, context)
             hs.append(h)
-        h = self.middle_block(h, emb, context)
-        for module in self.output_blocks:
+        # h = self.middle_block(h, emb, context)
+        h = self.middle_block(h)
+        for i, module in enumerate(self.output_blocks):
+            # if i in del_decode_idxs:
+            #     continue
             h = th.cat([h, hs.pop()], dim=1)
             h = module(h, emb, context)
         h = h.type(x.dtype)
@@ -724,6 +738,15 @@ class UNetModel(nn.Module):
             return self.id_predictor(h)
         else:
             return self.out(h)
+
+    def _reform(self):
+        del_encode_idxs = [6, 7, 8, 9, 10, 11]
+        del_decode_idxs = [0, 1, 2, 3, 4, 5]
+        for i in reversed(sorted(del_encode_idxs)):
+            del self.input_blocks[i]
+        for i in reversed(sorted(del_decode_idxs)):
+            del self.output_blocks[i]
+
 
 
 def disabled_train(self, mode=True):
@@ -2190,7 +2213,7 @@ def get_parser(**parser_kwargs):
         "--train",
         type=str2bool,
         const=True,
-        default=False,
+        default=True,
         nargs="?",
         help="train",
     )
@@ -2253,53 +2276,273 @@ def nondefault_trainer_args(opt):
 
 if __name__ == "__main__":
     now = datetime.datetime.now(timezone('Asia/Seoul')).strftime("%Y-%m-%d-%H-%M-%S")
-    sys.path.append(os.getcwd())
-    cfn = Path(os.path.basename(__file__)).stem
-    path_config = join("configs/", f"{cfn}.yaml")
 
-    # load configuration
-    config = OmegaConf.load(path_config)
-    config.model.target = config.model.target.format(cfn)
-    config.model.params.unet_config.target= config.model.params.unet_config.target.format(cfn)
+    # add cwd for convenience and to make classes in this file available when
+    # running as `python main.py`
+    # (in particular `main.DataModuleFromConfig`)
+    sys.path.append(os.getcwd())
+
+    parser = get_parser()
+    parser = Trainer.add_argparse_args(parser)
+
+    opt, unknown = parser.parse_known_args()
+    cfn = Path(os.path.basename(__file__)).stem
+    opt.base = [join("configs/", f"{cfn}.yaml")]
 
     # assert opt.name
-    cfg_fname = os.path.split(path_config)[-1]
+    cfg_fname = os.path.split(opt.base[0])[-1]
     cfg_name = os.path.splitext(cfg_fname)[0]
     nowname = f"{cfg_name}_{now}"
-    logdir = os.path.join("logs", nowname)
+    logdir = os.path.join(opt.logdir, nowname)
+    ckpt = os.path.join(logdir, "checkpoints", "last.ckpt")
+    resume = False
+
+    if os.path.isfile(ckpt):
+        opt.resume_from_checkpoint = ckpt
+        base_configs = sorted(glob.glob(os.path.join(logdir,
+                                                     "configs/*.yaml")))
+        opt.base = base_configs + opt.base
+        _tmp = logdir.split("/")
+        nowname = _tmp[-1]
+        resume = True
+
+    ckptdir = os.path.join(logdir, "checkpoints")
+    cfgdir = os.path.join(logdir, "configs")
+
     os.makedirs(logdir, exist_ok=True)
+    os.makedirs(ckptdir, exist_ok=True)
+    os.makedirs(cfgdir, exist_ok=True)
 
-    # model
-    model = instantiate_from_config(config.model)
-    model.cuda()
-    model.eval()
+    try:
+        # init and save configs
+        configs = [OmegaConf.load(cfg) for cfg in opt.base]
+        cli = OmegaConf.from_dotlist(unknown)
+        config = OmegaConf.merge(*configs, cli)
 
-    # data
-    data = instantiate_from_config(config.data)
-    data.prepare_data()
-    data.setup()
+        config.model.target = config.model.target.format(cfn)
+        config.model.params.unet_config.target= config.model.params.unet_config.target.format(cfn)
 
-    dataset = data.datasets['validation']
-    batch_size = 20
-    dataloader = DataLoader(dataset, batch_size=batch_size)
-    use_ddim = False
-    num_step = 25
-    with torch.no_grad(), model.ema_scope():
-        for i, value in enumerate(tqdm(dataloader)):
-            input = value['edit']['c_concat'].add(1).mul(0.5)
-            cond = value['edit']
-            cond['c_concat'] = [model.encode_first_stage(cond['c_concat'].cuda()).mode() * model.scale_factor]
-            x, _ = model.sample_log(
-                cond,
-                batch_size,
-                use_ddim,
-                num_step,
+        if resume:
+            # By default, when finetuning from Stable Diffusion, we load the EMA-only checkpoint to initialize all weights.
+            # If resuming InstructPix2Pix from a finetuning checkpoint, instead load both EMA and non-EMA weights.
+            config.model.params.load_ema = True
+
+        lightning_config = config.pop("lightning", OmegaConf.create())
+        # merge trainer cli with config
+        trainer_config = lightning_config.get("trainer", OmegaConf.create())
+        # default to ddp
+        trainer_config["accelerator"] = "ddp"
+        for k in nondefault_trainer_args(opt):
+            trainer_config[k] = getattr(opt, k)
+        if not "gpus" in trainer_config:
+            del trainer_config["accelerator"]
+            cpu = True
+        else:
+            gpuinfo = trainer_config["gpus"]
+            print(f"Running on GPUs {gpuinfo}")
+            cpu = False
+        trainer_opt = argparse.Namespace(**trainer_config)
+        lightning_config.trainer = trainer_config
+
+        # model
+        model = instantiate_from_config(config.model)
+        # model.model.diffusion_model.reform()
+
+        # trainer and callbacks
+        trainer_kwargs = dict()
+
+        # default logger configs
+        default_logger_cfgs = {
+            "testtube": {
+                "target": "pytorch_lightning.loggers.TestTubeLogger",
+                "params": {
+                    "name": "testtube",
+                    "save_dir": logdir,
+                }
+            },
+        }
+        default_logger_cfg = default_logger_cfgs["testtube"]
+        if "logger" in lightning_config:
+            logger_cfg = lightning_config.logger
+        else:
+            logger_cfg = OmegaConf.create()
+        logger_cfg = OmegaConf.merge(default_logger_cfg, logger_cfg)
+        trainer_kwargs["logger"] = instantiate_from_config(logger_cfg)
+
+        # modelcheckpoint - use TrainResult/EvalResult(checkpoint_on=metric) to
+        # specify which metric is used to determine best models
+        default_modelckpt_cfg = {
+            "target": "pytorch_lightning.callbacks.ModelCheckpoint",
+            "params": {
+                "dirpath": ckptdir,
+                "filename": "{epoch:06}",
+                "verbose": True,
+                "save_last": False,
+            }
+        }
+
+        if "modelcheckpoint" in lightning_config:
+            modelckpt_cfg = lightning_config.modelcheckpoint
+        else:
+            modelckpt_cfg = OmegaConf.create()
+        modelckpt_cfg = OmegaConf.merge(default_modelckpt_cfg, modelckpt_cfg)
+        print(f"Merged modelckpt-cfg: \n{modelckpt_cfg}")
+        if version.parse(pl.__version__) < version.parse('1.4.0'):
+            trainer_kwargs["checkpoint_callback"] = instantiate_from_config(
+                modelckpt_cfg)
+
+        # add callback which sets up log directory
+        default_callbacks_cfg = {
+            "setup_callback": {
+                "target": "etc.SetupCallback",
+                "params": {
+                    "resume": opt.resume,
+                    "now": now,
+                    "logdir": logdir,
+                    "ckptdir": ckptdir,
+                    "cfgdir": cfgdir,
+                    "config": config,
+                    "lightning_config": lightning_config,
+                }
+            },
+            "image_logger": {
+                "target": "exps.T001-A0A.ImageLogger",
+                "params": {
+                    "batch_frequency": 750,
+                    "max_images": 4,
+                    "clamp": True
+                }
+            },
+            # "learning_rate_logger": {
+            #     "target": "LearningRateMonitor",
+            #     "params": {
+            #         "logging_interval": "step",
+            #         # "log_momentum": True
+            #     }
+            # },
+            "cuda_callback": {
+                "target": "etc.CUDACallback"
+            },
+        }
+        if version.parse(pl.__version__) >= version.parse('1.4.0'):
+            default_callbacks_cfg.update(
+                {'checkpoint_callback': modelckpt_cfg})
+
+        if "callbacks" in lightning_config:
+            callbacks_cfg = lightning_config.callbacks
+        else:
+            callbacks_cfg = OmegaConf.create()
+
+        print(
+            'Caution: Saving checkpoints every n train steps without deleting. This might require some free space.'
+        )
+        default_metrics_over_trainsteps_ckpt_dict = {
+            'metrics_over_trainsteps_checkpoint': {
+                "target": 'pytorch_lightning.callbacks.ModelCheckpoint',
+                'params': {
+                    "dirpath": os.path.join(ckptdir, 'trainstep_checkpoints'),
+                    "filename": "{epoch:06}-{step:09}",
+                    "verbose": True,
+                    'save_top_k': -1,
+                    'every_n_train_steps': 5000,
+                    'save_weights_only': True
+                }
+            }
+        }
+        default_callbacks_cfg.update(default_metrics_over_trainsteps_ckpt_dict)
+
+        callbacks_cfg = OmegaConf.merge(default_callbacks_cfg, callbacks_cfg)
+        if 'ignore_keys_callback' in callbacks_cfg and hasattr(
+                trainer_opt, 'resume_from_checkpoint'):
+            callbacks_cfg.ignore_keys_callback.params[
+                'ckpt_path'] = trainer_opt.resume_from_checkpoint
+        elif 'ignore_keys_callback' in callbacks_cfg:
+            del callbacks_cfg['ignore_keys_callback']
+
+        trainer_kwargs["callbacks"] = [
+            instantiate_from_config(callbacks_cfg[k]) for k in callbacks_cfg
+        ]
+
+        trainer = Trainer.from_argparse_args(
+            trainer_opt,
+            plugins=DDPPlugin(find_unused_parameters=True),
+            **trainer_kwargs)
+        trainer.logdir = logdir  ###
+
+        # data
+        data = instantiate_from_config(config.data)
+        data.prepare_data()
+        data.setup()
+        print("#### Data #####")
+        for k in data.datasets:
+            print(
+                f"{k}, {data.datasets[k].__class__.__name__}, {len(data.datasets[k])}"
             )
-            x = model.decode_first_stage(x)
-            x = x.add(1).mul(0.5).clamp(0, 1)
 
-            for j, item in enumerate(range(len(x))):
-                img_in = ToPILImage()(input[j])
-                img_in.save(join(logdir, f"{i*batch_size+j:04d}_input.png"))
-                img_out = ToPILImage()(x[j])
-                img_out.save(join(logdir, f"{i*batch_size+j:04d}_output.png"))
+        # configure learning rate
+        bs, base_lr = config.data.params.batch_size, config.model.base_learning_rate
+        if not cpu:
+            ngpu = len(lightning_config.trainer.gpus.strip(",").split(','))
+        else:
+            ngpu = 1
+        if 'accumulate_grad_batches' in lightning_config.trainer:
+            accumulate_grad_batches = lightning_config.trainer.accumulate_grad_batches
+        else:
+            accumulate_grad_batches = 1
+        print(f"accumulate_grad_batches = {accumulate_grad_batches}")
+        lightning_config.trainer.accumulate_grad_batches = accumulate_grad_batches
+        if opt.scale_lr:
+            model.learning_rate = accumulate_grad_batches * ngpu * bs * base_lr
+            print(
+                "Setting learning rate to {:.2e} = {} (accumulate_grad_batches) * {} (num_gpus) * {} (batchsize) * {:.2e} (base_lr)"
+                .format(model.learning_rate, accumulate_grad_batches, ngpu, bs,
+                        base_lr))
+        else:
+            model.learning_rate = base_lr
+            print("++++ NOT USING LR SCALING ++++")
+            print(f"Setting learning rate to {model.learning_rate:.2e}")
+
+        # allow checkpointing via USR1
+        def melk(*args, **kwargs):
+            # run all checkpoint hooks
+            if trainer.global_rank == 0:
+                print("Summoning checkpoint.")
+                ckpt_path = os.path.join(ckptdir, "last.ckpt")
+                trainer.save_checkpoint(ckpt_path)
+
+        def divein(*args, **kwargs):
+            if trainer.global_rank == 0:
+                import pudb
+                pudb.set_trace()
+
+        import signal
+
+        signal.signal(signal.SIGUSR1, melk)
+        signal.signal(signal.SIGUSR2, divein)
+
+        # run
+        if opt.train:
+            try:
+                trainer.fit(model, data)
+            except Exception:
+                # melk()
+                raise
+        if not opt.no_test and not trainer.interrupted:
+            trainer.test(model, data)
+    except Exception:
+        if opt.debug and trainer.global_rank == 0:
+            try:
+                import pudb as debugger
+            except ImportError:
+                import pdb as debugger
+            debugger.post_mortem()
+        raise
+    finally:
+        # move newly created debug project to debug_runs
+        if opt.debug and not opt.resume and trainer.global_rank == 0:
+            dst, name = os.path.split(logdir)
+            dst = os.path.join(dst, "debug_runs", name)
+            os.makedirs(os.path.split(dst)[0], exist_ok=True)
+            os.rename(logdir, dst)
+        if trainer.global_rank == 0:
+            print(trainer.profiler.summary())
